77b5ef5e3c0b0549044e84516e281279ae2c6a89
Minato Fukuda <minatofukuda@gmail.com>
2022-01-02 22:28:01 -0500

    Retreiving image URLs from liked tweets

--- requestimages.py
+++ requestimages.py
@@ -1,8 +1,23 @@
 import os
 import requests
+from requests.models import HTTPError
+from pydrive.auth import GoogleAuth
 
-BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
-r = {'Authorization' : f'Bearer {BEARER_TOKEN}', 'User-Agent' : 'v2LikedTweetsPython'}
-print(r['Authorization'])
-print(r['User-Agent'])
-url = 'https://api.twitter.com/2/users/756179614154756096/liked_tweets'
+def get_images():
+    BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+    headers = {'Authorization' : f'Bearer {BEARER_TOKEN}', 'User-Agent' : 'v2LikedTweetsPython'}
+    params = {'max_results' : '5', 'expansions' : 'attachments.media_keys', 'media.fields' : 'url'}
+    url = 'https://api.twitter.com/2/users/756179614154756096/liked_tweets'
+    try:
+        res = requests.get(url, headers=headers, params=params)
+        res.raise_for_status()
+        print(res.json()['includes'])
+        return res.json()['includes']
+    except HTTPError as err:
+        print(err)
+
+def upload_images():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() 
+
+upload_images()
